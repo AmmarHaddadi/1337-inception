f9d2f0db49d47da5a7166a0b535ee295641eae3f
Ammar <75220043+AmmarHaddadi@users.noreply.github.com>
2026-06-18 11:43:04 +0000

    updates

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ COMPOSE_FILE  = src/docker-compose.yml
 # change this
 DATA_DIR      = /workspaces/codespaces-blank/data
 
-default: up
+default: up-watch
 
 up: create_dirs
 	docker compose -f $(COMPOSE_FILE) up --build -d
+
+up-watch: create_dirs
+	docker compose -f $(COMPOSE_FILE) up --build 
 
 down:
 	docker compose -f $(COMPOSE_FILE) down
@@ -28,5 +31,6 @@ re: fclean up
 create_dirs:
 	@mkdir -p $(DATA_DIR)/maria
 	@mkdir -p $(DATA_DIR)/wordpress
+	@chmod 777 $(DATA_DIR)/*
 
 .PHONY: all up down clean fclean re create_dirs
--- src/wordpress/entry.sh
+++ src/wordpress/entry.sh
@@ -2,30 +2,32 @@
 
 set -e
 
-echo "Waiting for maria to start"
-while ! mysqladmin ping -h"mariadb" --silent; do
-    sleep 1
-done
+# echo "Waiting for maria to start"
+# while ! mysqladmin ping -h"mariadb" --silent; do
+#     sleep 1
+# done
 
 mkdir -p /var/www/html
 chown -R www-data:www-data /var/www/html
 
 if [ ! -f "wp-config.php" ]; then
     echo "installing wordpress"
-    wp core download --allow-root
+    # wp core download --allow-root
+    php -d memory_limit=-1 /usr/local/bin/wp core download --allow-root # should solve memory issues
 
     DB_PASS=$(cat /run/secrets/maria_normal)
     WP_ADMIN_PASS=$(cat /run/secrets/maria_root) 
     WP_USER_PASS=$(cat /run/secrets/wp_user_pass)
 
-    wp config create \
+    php -d memory_limit=-1 /usr/local/bin/wp config create \
         --dbname=wordpress \
         --dbuser="$MARIA_USERNAME" \
         --dbpass="$DB_PASS" \
         --dbhost="mariadb:3306" \
         --allow-root
 
-    wp core install \
+
+    php -d memory_limit=-1 /usr/local/bin/wp core install \
         --url="${DOMAIN_NAME}" \
         --title="${WP_TITLE}" \
         --admin_user="${WP_ADMIN_USER}" \
@@ -33,12 +35,17 @@ if [ ! -f "wp-config.php" ]; then
         --admin_email="admin@${DOMAIN_NAME}" \
         --allow-root
 
-    wp user create \
+    php -d memory_limit=-1 /usr/local/bin/wp user create \
         "${WP_USER}" \
         "user@${DOMAIN_NAME}" \
         --user_pass="$WP_USER_PASS" \
         --role=author \
         --allow-root
+
+    # temporary codespace fix
+    php -d memory_limit=-1 /usr/local/bin/wp config set WP_HOME "https://${DOMAIN_NAME}" --allow-root
+    php -d memory_limit=-1 /usr/local/bin/wp config set WP_SITEURL "https://${DOMAIN_NAME}" --allow-root
+    php -d memory_limit=-1 /usr/local/bin/wp config set --raw HTTP_X_FORWARDED_PROTO "'https'" --allow-root
         
    
     chown -R www-data:www-data /var/www/html
